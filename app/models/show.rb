6c674be38f1472189d44bc7d2bb2a031425e73d2
Margot Konig <margot.konig@blackrock.com>
2019-06-20 18:27:59 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   
   def self.most_popular_show
     rating = Show.maximum(:rating)
-    Show.where("rating = ?",rating)
+    Show.where("rating == ?",rating)
   end 
   
   def self.lowest_rating 
