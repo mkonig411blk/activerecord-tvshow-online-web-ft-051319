0d7ff5c546f67a90a146aa09b3cc5f6840fd35aa
Margot Konig <margot.konig@blackrock.com>
2019-06-20 18:08:51 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,29 @@
 class Show < ActiveRecord::Base 
+  
+  def highest_rating 
+    show = Show.maximum(:rating)
+    show.rating
+  end 
+  
+  def most_popular_show
+    Show.maximum(:rating)
+  end 
+  
+  def lowest_rating 
+    show = Show.minimum(:rating)
+    show.rating
+  end 
+  
+  def least_popular_show 
+    Show.minimum(:rating)
+  end 
+  
+  def ratings_sum
+  end 
+  
+  def popular_shows
+  end 
+  
+  def shows_by_alphabetical_order 
+  end 
 end
